c71bee1abae685cb216943a81c536bea8d8e0724
Aidan Pieper <aidanpieper@localprojects.com>
2016-09-01 16:27:42 -0400

    Working databinding!

--- app/src/main/kotlin/io/github/plastix/stride/ui/main/MainViewModel.kt
+++ app/src/main/kotlin/io/github/plastix/stride/ui/main/MainViewModel.kt
@@ -1,10 +1,31 @@
 package io.github.plastix.stride.ui.main
 
 import android.content.Context
+import android.databinding.BaseObservable
+import android.databinding.Bindable
+import io.github.plastix.stride.BR
 import io.github.plastix.stride.data.DataManager
 import io.github.plastix.stride.ui.ActivityScope
+import rx.Observable
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @ActivityScope
-class MainViewModel @Inject constructor(@ActivityScope val context: Context, val dataManager: DataManager) {
+class MainViewModel @Inject constructor(@ActivityScope val context: Context, val dataManager: DataManager) : BaseObservable() {
+
+    var steps: String = ""
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.steps)
+        }
+
+
+    init {
+        Observable.interval(1, TimeUnit.SECONDS)
+                .map { it.toString() }
+                .subscribe { steps = it }
+    }
+
+
 }
